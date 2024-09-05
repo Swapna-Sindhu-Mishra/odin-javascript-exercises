b0cd87cecdfbadf2d5a0ceed62f36e6a99c159e3
Swapna Sindhu Mishra <48241258+Swapna-Sindhu-Mishra@users.noreply.github.com>
2024-09-05 19:43:35 +0200

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var newstring = '';
+    if (num<0) {
+        return 'ERROR';
+    } else {
+        for (let i=0; i<num; i++) {
+            newstring += string;
+          }
+          return newstring;
+    }
 };
+
+repeatString('goodbye', -1);
 
 // Do not edit below this line
 module.exports = repeatString;
