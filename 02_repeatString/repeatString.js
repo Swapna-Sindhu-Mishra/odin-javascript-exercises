bb8a1d10b55919e20e1c17fa28016385c340602b
Swapna Sindhu Mishra <48241258+Swapna-Sindhu-Mishra@users.noreply.github.com>
2024-09-09 22:56:01 +0200

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,12 +2,11 @@ const repeatString = function(string, num) {
     var newstring = '';
     if (num<0) {
         return 'ERROR';
-    } else {
-        for (let i=0; i<num; i++) {
-            newstring += string;
-          }
-          return newstring;
     }
+    for (let i=0; i<num; i++) {
+        newstring += string;
+        }
+    return newstring;
 };
 
 repeatString('goodbye', -1);
